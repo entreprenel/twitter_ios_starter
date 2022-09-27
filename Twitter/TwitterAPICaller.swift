2ba25d5108b663d366a6c7dedd0dbaf7441d5259
Enel Elas <enel@nxtbloc.io>
2022-09-27 09:43:01 -0500

    Twiiter changes

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -9,8 +9,10 @@
 import UIKit
 import BDBOAuth1Manager
 
+
+
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "dm3KSIWoL3sN5IeMhIi8IBIOB", consumerSecret: "Ic5ARRftp5tpCURRvRAczRYfr8NnPEJrSrOqoEQ3BWEJRYRA4O")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
